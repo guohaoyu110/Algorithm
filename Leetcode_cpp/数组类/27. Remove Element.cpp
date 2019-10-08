71b3c960cb2d52e3ce421a7421da7ff341cc79a3
guohaoyu110 <guohaoyu110@gmail.com>
2019-10-08 12:38:33 -0700

    new 20191008

--- Leetcode_cpp/数组类/27. Remove Element.cpp
+++ Leetcode_cpp/数组类/27. Remove Element.cpp
@@ -11,3 +11,22 @@ public:
         return nums1.size();
     }
 };
+
+//第二种方法，时间复杂度和空间复杂度都会更好一些。
+class Solution{
+public:
+    int removeElement(vector<int>& nums, int val)
+    {
+        //必须原地修改
+        int len = 0;
+        for(int i =0;i<nums.size();i++)
+        {
+            //nums[len++] = 
+            if (nums[i]!=val)
+            {
+                nums[len++] = nums[i];
+            }
+        }
+        return len;
+    }
+};
