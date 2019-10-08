a1b799cbb660a232a93bb4c138c7f9733952dc2e
guohaoyu110 <guohaoyu110@gmail.com>
2019-10-08 13:44:00 -0700

    new 20191008

--- Leetcode_cpp/数组类/31. Next Permutation.cpp
+++ Leetcode_cpp/数组类/31. Next Permutation.cpp
@@ -60,3 +60,28 @@ public:
         }  
     }
 };
+
+//方法三：这样写更好一些
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r)
+        {
+            int mid = l + (r - l >> 2); 
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[r]) // 右半边有序
+            {
+                if (nums[mid] < target && nums[r] >= target) l = mid + 1;
+                else r = mid - 1;
+            }
+          
+            else// 左半边有序
+            {
+                if (nums[l] <= target && nums[mid] > target) r = mid - 1;
+                else l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
--- Leetcode_cpp/数组类/33. Search-in-rotated-sorted-array.cpp
+++ Leetcode_cpp/数组类/33. Search-in-rotated-sorted-array.cpp
@@ -19,3 +19,40 @@ public:
     }
 };
 //第二种方法，考虑如何达到要求的时间复杂度
+//第二种方法，考虑如何达到要求的时间复杂度
+//这个复杂度要求，很明显要用二分法
+//不能同时做i++，j--;容易出问题
+class Solution{
+public:
+    int search(vector<int>& nums, int target)
+    {
+        
+        int i=0,j=nums.size()-1;
+        if (nums.size()==0) return -1;
+        if (nums.size()==1&&target == nums[0])
+            return 0;
+        if (nums.size()>=2){
+        while(i<j)
+        {
+            if(target < nums[i] && target < nums[j])
+            {
+                //i++;
+                j--;
+            }
+            if (target == nums[i])
+                return i;
+            if (target == nums[j])
+                return j;
+            if (target > nums[i] && target > nums[j])
+            {
+                i++;//j--;
+            }
+            if (target > nums[i]&& target < nums[j])
+                i++;
+            if (target < nums[i]&& target > nums[j])
+                j--;
+         
+        }}
+        return -1;
+    }
+};
