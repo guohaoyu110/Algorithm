70d2ca70171de393f0aa4cfeff11c07e73bf65f6
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-25 11:33:45 -0800

    new 20191225

---
+++ AcWing_cpp/154.滑动窗口.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+const int N = 1e6 + 10;
+int a[N], q[N], hh, tt = -1;
+
+
+int main()
+{
+    //int hh=0,tt =-1;
+    ios::sync_with_stdio(false);
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if (i-k+1>q[hh]) 
+            ++hh; // hh+1
+        while(hh<=tt&&a[i]<=a[q[tt]])
+            --tt;
+        q[++tt] = i;
+        if (i+1>=k) cout<<a[q[hh]]<<" ";
+    }
+    cout<<endl;
+    hh = 0, tt = -1;
+    for (int i = 0; i < n; ++ i)
+    {
+        if (i - k + 1 > q[hh]) 
+            ++ hh;
+        while (hh <= tt && a[i] >= a[q[tt]]) 
+            -- tt;
+        q[++ tt] = i;
+        if (i + 1 >= k) 
+            printf("%d ", a[q[hh]]);
+    }
+    return 0;
+}
