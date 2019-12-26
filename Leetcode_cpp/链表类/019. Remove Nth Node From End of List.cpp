f76ac9802baa5997f7dbad9977a96a8997e938a9
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-25 22:45:41 -0800

    new 20191225

---
+++ Leetcode_cpp/链表类/019. Remove Nth Node From End of List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//这道题目可以扫描一遍，也可以扫描两遍；扫描一遍需要用两个指针！
+//删除倒数第k个节点
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        auto p1 = head, p2 = head;
+        for (int i=0;i<n;i++)
+        {
+            p1 = p1->next;
+        }
+        if (p1 == NULL) return p2->next;
+        while(p1->next!=NULL)
+        {
+            p1 = p1->next;p2 = p2->next;
+        }
+        p2->next = p2->next->next;
+        return head;
+    }
+};
