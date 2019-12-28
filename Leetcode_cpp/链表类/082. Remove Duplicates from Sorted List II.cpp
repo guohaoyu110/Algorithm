63c3f1caedf9d1c8e207ba7171be0465e56b9551
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-27 18:03:32 -0800

    new 20191227

--- Leetcode_cpp/链表类/082. Remove Duplicates from Sorted List II.cpp
+++ Leetcode_cpp/链表类/082. Remove Duplicates from Sorted List II.cpp
@@ -9,7 +9,17 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        
-        
+        //这个因为头结点是可能被删除的，所以必须加上一个虚拟节点
+        auto dummy = new ListNode(-1);
+        dummy->next = head;
+        auto p = dummy;
+        while(p->next){
+            auto q = p->next;
+            while(q&&q->val == p->next->val)
+                q = q->next;
+            if (p->next->next == q) p = p->next;
+            else p->next = q;
+        }
+        return dummy->next;//同样因为头结点可能被删除所有,所以不能return head，其他情况都是没有问题的
     }
 };
