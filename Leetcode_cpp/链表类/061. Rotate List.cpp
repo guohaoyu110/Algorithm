3342202acecba57d90b939e774b4e26a988929df
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-27 20:23:17 -0800

    new 20191227

--- Leetcode_cpp/链表类/061. Rotate List.cpp
+++ Leetcode_cpp/链表类/061. Rotate List.cpp
@@ -10,8 +10,32 @@ class Solution {
 public:
 //这个肯定是要两个指针的，一个快指针，一个慢指针
 //时间复杂度分析：链表一共遍历2遍，所以总时间复杂度是 O(n)O(n)。
+//这道题目还是要很小心的！
     ListNode* rotateRight(ListNode* head, int k) {
-        
+        //这个没有必要创建一个虚节点
+        if (head == NULL) return head;
+        auto p = head;
+        int n = 0;
+        while(p){
+            n++;
+            p = p->next;
+        }
+        if (!k) return head;
+        k = k%n;
 
+        auto first = head;
+        while(k--&&first) first = first->next;
+        auto second = head;
+
+        while(first->next){
+            first = first->next;
+            second = second->next;
+        }
+        first->next = head;
+        head = second->next;
+        second->next = NULL;
+
+
+        return head;
     }
 };
