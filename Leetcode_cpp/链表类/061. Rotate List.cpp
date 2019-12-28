b5525f0059e589f992e489006ea09af7a24b9647
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-27 20:04:17 -0800

    new 20191227

---
+++ Leetcode_cpp/链表类/061. Rotate List.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+//这个肯定是要两个指针的，一个快指针，一个慢指针
+//时间复杂度分析：链表一共遍历2遍，所以总时间复杂度是 O(n)O(n)。
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+
+    }
+};
--- Leetcode_cpp/链表类/206. Reverse Linked List.cpp
+++ Leetcode_cpp/链表类/206. Reverse Linked List.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         //auto dummy = new ListNode(-1);
-        ListNode *prev = NULL;
+        ListNode *prev = NULL; //prev 表示前继节点
         while(head !=NULL)
         {
             //temp记录下一个节点，head是当前节点
@@ -18,6 +18,7 @@ public:
             head->next = prev;
             prev = head;
             head = tmp;
+            //这个思维还是精巧的，多看看这道题目！
         }
         return prev;
 
