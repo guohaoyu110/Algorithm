1ea2e7a78929b011865ea98ade22470084a77f7a
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-28 17:06:07 -0800

    new 20191228

---
+++ Leetcode_cpp/链表类/876. Middle of the Linked List.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        //这个还是一个快指针，一个慢指针的方法
+        auto first = head;
+        auto second = head;
+        while (first&& first->next) {
+            //slow = slow->next;
+            //fast = fast->next->next;
+            second = second->next;
+            first = first->next->next;
+        }
+        //return slow;
+
+        return second;
+    }
+};
