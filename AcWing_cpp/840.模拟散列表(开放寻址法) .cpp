de8a82a6d14b9dffabc1a6489e2d152562a299b8
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-30 12:43:46 -0800

    new 20191230

--- AcWing_cpp/840.模拟散列表(开放寻址法) .cpp
+++ AcWing_cpp/840.模拟散列表(开放寻址法) .cpp
@@ -4,14 +4,16 @@
 using namespace std;
 const int N = 200003;//一定要找一个在这个大概数据范围的质数
 
-int h[N],null = 0x3f3f3f3f;
+int h[N],null = 0x3f3f3f3f; //−109≤x≤109 null是大于10的9次方这个范围的！
+//这个null一定要保证不在这个数据范围里面 
 //如果x在哈希表中，返回x的下标；如果x不在哈希表中，返回x应该插入的位置
+//寻找下一个坑位的方法！
 int find(int x)
 {
     int t = (x%N+N)%N;
     while(h[t]!=null && h[t]!=x ){
         t++;
-        if (t== N) t = 0;//这个是循环的
+        if (t== N) t = 0;//k找到最后一个位置了，就从头开始找，这是一个循环的过程。
     }
     return t;
 }
