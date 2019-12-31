50adf0407dafc883278dbb8d6810a48c533c77e1
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-31 14:43:41 -0800

    new 20191231

--- AcWing_cpp/021. 斐波那契数列.cpp
+++ AcWing_cpp/021. 斐波那契数列.cpp
@@ -12,4 +12,44 @@ public:
     }
 };
 
-//方法二：
+//方法二：21ms 这样写的话，快很多！
+class Solution {
+public:
+    int v[100000] = { 0 };
+
+    int Fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n ==2) {
+            v[n] = 1;
+            return v[n];
+        }
+        if (v[n] != 0) return v[n];
+        for (int i = 3; i <= n; i++) {
+            v[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        return v[n];
+    }
+};
+//方法三：694ms
+class Solution {
+public:
+    int v[100000] = { 0 };
+
+    int Fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n ==2) {
+            v[n] = 1;
+            return v[n];
+        }
+        //if (v[n] != 0) return v[n]; //这一步还是很重要的
+        if (n>=3){
+            v[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        return v[n];
+    }
+};
+
+
+
