81019f738583799b9a1d6677ab395b0cd5ced74a
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-30 22:01:28 -0800

    new 20191230

--- AcWing_cpp/841. 字符串哈希.cpp
+++ AcWing_cpp/841. 字符串哈希.cpp
@@ -33,13 +33,39 @@ int main()
     return 0;
 }
 '''
-
+// 之前那种两种循环的复杂度太高，可以用哈希的方式
 #include <iostream>
 #include <cstring>
 using namespace std;
+typedef unsigned long long ULL;
+const int N = 1e5+10;
+ULL h[N],p[N]; //h[k] 存储字符串前k个字母的哈希值，p[k]存储p^k mod 2 ^64
+char str[N];
+
+const int P = 131;
+
+ULL get (int l, int r)
+{
+    return h[r] - h[l-1]*p[r-l+1];
+}
 
 int main()
 {
+    int n,m;
+    cin>>n>>m;
     
+    scanf("%s",str+1);
+    p[0] = 1;
+    for (int i = 1;i<=n;i++)
+    {
+        h[i] = h[i-1]*P + str[i];
+        p[i] = p[i-1]*P;
+    }
+     while(m--){
+        int l1,r1,l2,r2;
+        cin>>l1>>r1>>l2>>r2;
+        if (get(l1,r1)==get(l2,r2)) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
     return 0;
 }
