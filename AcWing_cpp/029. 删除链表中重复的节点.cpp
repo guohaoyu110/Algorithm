f649363c9ae37380bf7dce66f33aeb65d05706fc
guohaoyu110 <guohaoyu110@gmail.com>
2019-12-31 23:17:43 -0800

    new 20191231

--- AcWing_cpp/029. 删除链表中重复的节点.cpp
+++ AcWing_cpp/029. 删除链表中重复的节点.cpp
@@ -7,17 +7,28 @@
  * };
  */
 //这道题目还是值得思考的，为什么需要两个指针，为什么一个指针是不可以的？
-//
+// 因为隐藏掉的那个部分只能跳过一个值，不能跳过两个连续的值
+'''
+    while(p->next){
+        if (p->next->val == p->next->next->val)                
+            p->next = p->next->next;//用这种方法只能跳过一个数值，不能跳过两个连续的数值
+        else p = p->next;
+    }
+'''
+//链表题目一定要在纸上画清楚，想清楚每一个的过程！
 class Solution {
 public:
     ListNode* deleteDuplication(ListNode* head) {
-        auto p = new ListNode(-1);
-        p->next = head;
+        auto dummy = new ListNode(-1);
+        dummy->next = head;
+        auto p = dummy;
         while(p->next){
-            if (p->next->val == p->next->next->val)
-                p->next = p->next->next;
-            else p = p->next;
+            auto q = p->next;
+            while(q&&p->next->val == q->val) q = q->next;
+            if (p->next->next == q) p = p->next;
+            else p->next = q;
+
         }
-        return head;
+        return dummy->next;
     }
 };
