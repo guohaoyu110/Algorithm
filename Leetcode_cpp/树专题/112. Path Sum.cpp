ac558ec6c1c3aff0a65f259d1048949c9fe08ba4
guohaoyu110 <guohaoyu110@gmail.com>
2020-01-03 11:09:41 -0800

    new 20200103

---
+++ Leetcode_cpp/树专题/112. Path Sum.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (!root) return false;
+        if (!root->left && !root->right) return root->val == sum;//这个极端情况，如果root节点就是等于sum
+        if (root->left&&hasPathSum(root->left,sum - root->val)) return true;
+        if (root->right && hasPathSum(root->right, sum - root->val)) return true;
+        return false;
+    }
+};
