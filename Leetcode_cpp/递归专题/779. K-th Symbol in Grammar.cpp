a928669843a1ecbbacdd4509d4b4202d20b9d9c2
guohaoyu110 <guohaoyu110@gmail.com>
2020-01-05 17:46:39 -0800

    new 20200105

--- Leetcode_cpp/递归专题/779. K-th Symbol in Grammar.cpp
+++ Leetcode_cpp/递归专题/779. K-th Symbol in Grammar.cpp
@@ -1,6 +1,21 @@
+//方法一：
 class Solution {
 public:
     int kthGrammar(int N, int K) {
-        
+        int n;
+        for (n = 0, K -= 1; K ; K &= (K - 1)){  //求解K-1的二进制中1的个数
+            n++;
+        }
+        return n & 1;
+    }
+};
+
+//方法二：
+
+class Solution {
+public:
+    int kthGrammar(int N, int K) {
+        if (N == 1) return 0;
+        return kthGrammar(N-1, (K+1)/2)==0 ? (K-1)%2 : K%2;
     }
 };
