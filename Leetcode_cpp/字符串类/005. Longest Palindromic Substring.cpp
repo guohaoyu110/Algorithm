84469890bb220094522667234d2da65524431313
guohaoyu110 <guohaoyu110@gmail.com>
2020-01-11 11:38:40 -0800

    new 20200111

--- Leetcode_cpp/字符串类/005. Longest Palindromic Substring.cpp
+++ Leetcode_cpp/字符串类/005. Longest Palindromic Substring.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        //这个是不是一定要用双指针的办法？
         
+
     }
 };
 
--- Leetcode_cpp/树专题/144. Binary Tree Preorder Traversal前序遍历.cpp
+++ Leetcode_cpp/树专题/144. Binary Tree Preorder Traversal前序遍历.cpp
@@ -7,13 +7,16 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//用递归的方法时间复杂度大了一些
+//方法一：递归
 class Solution {
 public:
     vector<int>pre;
     vector<int> preorderTraversal(TreeNode* root) {
         //auto p = root;
         //vector<int>pre;
-        if (root){
+        if (root)//当前节点非空
+        {
             pre.push_back(root->val);
             preorderTraversal(root->left);
             preorderTraversal(root->right);
@@ -21,3 +24,22 @@ public:
         return pre;
     }
 };
+
+//方法二：不用递归，迭代的方法
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    stack<TreeNode*> stk;
+    stk.push(root);
+    while (!stk.empty()) {
+        TreeNode* node = stk.top();
+        stk.pop();
+        if(node) ans.push_back(node->val);
+        else continue;
+        if (node->right) stk.push(node->right);
+        if (node->left) stk.push(node->left);
+    }
+    return ans;
+}
+
+//方法三：用迭代的放啊
+
