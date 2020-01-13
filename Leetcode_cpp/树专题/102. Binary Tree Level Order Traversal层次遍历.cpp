9c19060c4a6787e9a49e9036caf7ecacfe5e4123
guohaoyu110 <guohaoyu110@gmail.com>
2020-01-12 23:03:49 -0800

    newe 20200112

--- Leetcode_cpp/树专题/102. Binary Tree Level Order Traversal层次遍历.cpp
+++ Leetcode_cpp/树专题/102. Binary Tree Level Order Traversal层次遍历.cpp
@@ -20,26 +20,64 @@
 
 时间复杂度分析：树中每个节点仅会进队出队一次，所以时间复杂度是 O(n) 。
 '''
+//方法一：这个思路还是非常简洁明了的
 class Solution {
 public:
+    vector<int> get_val(vector<TreeNode*> level)
+    {
+        vector<int> res;
+        for (auto &u : level)
+            res.push_back(u->val);
+        return res;
+    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> res;
+        vector<vector<int>> res;//这里是用数组代替队列了，如果用队列的话应该怎么写呢？看下面的解法
         if (!root) return res;
-        vector<TreeNode*>level;
+        vector<TreeNode*>level;//存储了这个整棵树
         level.push_back(root);
-        res.push_back()
+        res.push_back(get_val(level));
         while(true){
+            //while(true)在这里是必须的
             vector<TreeNode*>newlevel;
             for(auto u :level){
                 if (u->left) newlevel.push_back(u->left);
                 if (u->right) newlevel.push_back(u->right);
             }
             if (newlevel.size()){
-                res.push_back()
+                res.push_back(get_val(newlevel));
                 level = newlevel;
             }
             else break;
         }
         return res;
+    }
+};
+
+
+//方法二：用队列的方法
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void bfs(TreeNode* root, vector<vector<int>>& res){
+        queue<TreeNode*>s;
+        s.push(root);
+        s.push(NULL);
+        while(s.front()!=NULL){
+            vector<int>t;
+
+        }
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
     }
 };
--- Leetcode_cpp/树专题/106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ Leetcode_cpp/树专题/106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -8,6 +8,7 @@
  * };
  */
 //这是二叉树的很典型的题目，从中序和后序来构造二叉树
+//前序和后序是不好构造二叉树的
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
