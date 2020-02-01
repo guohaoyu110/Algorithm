7aac07420bf8fe74510fe8bc632cf3e198943bd6
guohaoyu110 <guohaoyu110@gmail.com>
2020-01-31 18:49:52 -0800

    new 20200131

--- Leetcode_cpp/数组类/001. Two Sum.cpp
+++ Leetcode_cpp/数组类/001. Two Sum.cpp
@@ -1,40 +1,55 @@
-// 暴力枚举 O(N^2)
+//方法一：最基础的解法
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        for(int i=0;i<nums.size();++i){
-        	for(int j=0;j<nums.size();++j){
-        		if(nums[i]+nums[j]==target){
-        			res = vector<int>({i,j});
-        			break;
-        		}
-        	}
-        	if(res.size()>0) break;
+        vector<int>nums1;
+        //nums1.push_bach()
+        int size = nums.size();
+        for (int i=0; i<size-1; i++)
+        {
+            for (int j=i+1;j<size;j++)
+            {
+                if (nums[i]+nums[j]==target)
+                {
+                    nums1.push_back(i);
+                    nums1.push_back(j);
+                    break;
+                }  
+            }
         }
+        return nums1;
     }
 };
 
-// hash 复杂度O(n)  unordered_map<int, int> hash
+
+//方法二：hashmap
+//关联容器是对容器概念的一个改进，将值与键关联在一起
+//可以降低一重时间复杂度
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
+    vector<int> twoSum(vector<int>& nums, int target) 
     {
-        vector<int> res;
-        unordered_map<int, int> hash；
-        for(int i=0;i<nums.size();++i)
+        unordered_map<int,int>m;
+        vector<int>res;
+        for (int i=0;i<nums.size();i++)
         {
-        	int another = target-nums[i];
-        	if(hash.count(another)){
-        		res=vector<int>({i,hash[another]});//hash[value]=pos
-        		break;
-        	}
-        	hash[nums[i]]=i
+            m[nums[i]]=i;
+        }
+        for (int j=0;j<nums.size();j++)
+        {
+            int t = target- nums[j];
+            if (m[t]!=j && m.count(t)){
+                res.push_back(j);
+                res.push_back(m[t]);
+                break;
+            }
+                
+            
         }
         return res;
     }
-};
+}; 
 
 
 // 20190824 update
